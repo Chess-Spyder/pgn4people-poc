737d6d91e5c161ee35aacc88ce1c8daab3f35cb5
Jim Ratliff <jim@jim-ratliff.name>
2022-05-17 16:42:22 -0700

    __main__.py now calls main()

--- src/pgn4people_poc/__main__.py
+++ src/pgn4people_poc/__main__.py
@@ -3,8 +3,8 @@ Entry point for pgn4people_poc package when executed as python -m pgn4people_poc
 See, e.g., https://docs.python.org/3/library/__main__.html
 """
 
-import pgnfocus
+from . pgn4people import main
 
-pgnfocus.pgnfocus()
+main()
 
 
