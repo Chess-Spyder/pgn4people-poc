9ce76ac8e9a5fba739265f8cf8f5c1bd7faaf477
Jim Ratliff <jim@jim-ratliff.name>
2022-05-28 13:50:15 -0700

    Wrap main() in sys.exit() in __main__.py

--- src/pgn4people_poc/__main__.py
+++ src/pgn4people_poc/__main__.py
@@ -3,9 +3,12 @@ Entry point for pgn4people_poc package when executed as python -m pgn4people_poc
 See, e.g., https://docs.python.org/3/library/__main__.html
 """
 
+import sys
+
 from . pgn4people import main
 
 
-main()
+if __name__ == "__main__":
+    sys.exit(main())
 
 
