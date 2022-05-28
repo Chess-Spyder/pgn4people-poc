336c8e41e3b1ce8e515989cccf0c59c7910b052f
Jim Ratliff <jim@jim-ratliff.name>
2022-05-28 13:47:04 -0700

    Implement single source of truth for version number, adding __version__.py file and importing it in __init__.py

--- src/pgn4people_poc/__init__.py
+++ src/pgn4people_poc/__init__.py
@@ -1,1 +1,7 @@
 """ Package pgn4people_poc """
+
+# The following import (from . __version__ import __version__) is a component
+# of the mechanism to provide a single source for the version number.
+# For more information, see the docstring comments in the __version__.py file.
+
+from . __version__ import __version__
---
+++ src/pgn4people_poc/__version__.py
@@ -0,0 +1,23 @@
+"""
+Provides a single source of truth for the version number. Modify the string
+`__version__ = '0.0.2'` to conform to the correct version number, that should
+be compliant with PEP 440.
+
+This mechanism for setting the version number also requires the following:
+1) That the __init__.py file in this directory have the snippet:
+
+    from . __version__ import __version__
+
+2) That the setup.cfg file in the project directory include the following
+snippet:
+
+    [metadata]
+    …
+    version = attr: pgn4people_poc.__version__
+
+Generally, see the answers to “Set `__version__` of module from a file when
+configuring setuptools using `setup.cfg` without `setup.py`,” Stack
+Overflow, May 23, 2022.
+https://stackoverflow.com/questions/72357031/set-version-of-module-from-a-file-when-configuring-setuptools-using-setup
+"""
+__version__ = '0.7.0'
