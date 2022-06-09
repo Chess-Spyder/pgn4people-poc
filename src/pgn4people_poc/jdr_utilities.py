fa46cdb204ae63156188b4716c5799dffe612201
Jim Ratliff <jim@jim-ratliff.name>
2022-06-09 15:47:23 -0700

    Replace check for blank line with method using regex
    Allows for whitespace on “blank-ish” line. Greedy implementation sucks up consecutive blank-ish lines.

--- src/pgn4people_poc/jdr_utilities.py
+++ src/pgn4people_poc/jdr_utilities.py
@@ -4,6 +4,8 @@ Checks argument on command line for a path to a user-supplied PGN file.
 
 import argparse
 import pathlib
+import re
+
 # Note: check_CLI_for_user_file belongs to jdr_utilities
 def check_CLI_for_user_file():
 
@@ -23,3 +25,119 @@ def check_CLI_for_user_file():
     user_text_filepath = args.user_text_filepath
 
     return user_text_filepath
+
+
+def id_text_between_first_two_blankish_lines (string_from_file):
+    """
+    Here, a “blank-ish” line is any of:
+    (a) a pair of consecutive newline characters,
+    (b) a newline character, followed by a string of only non-newline whitespace, followed by a newline character,
+    (c) a newline character, followed by a string of only non-newline whitespace, followed by an end of file
+
+    (As implemented, however, consecutive blank-ish lines are treated as a single blank-ish line.)
+
+    When passed string_from_file, this function determines the start and stop indices such that the slice
+    `string_from_file[start: end::]` contains exactly the text between the pair of blank-ish lines.
+
+    Returns the 2-tuple (start_index, end_index), where these are interpreted in the same way as a Python string slice.
+
+    If there is only an initial blank-ish line (or set of consecutive blank-ish lines), but not two, start_index will be
+    returned with the index of the beginning of the text after the blank-ish line, but end_index will be returned as
+    None. In this case, the desired text is a slice that begins at start_index but runs through the end of the string.
+
+    If the file is empty or otherwise has no blank-ish line, both elements of the 2-tuple will be returned as None.
+
+    Usage example:
+        (start_index, end_index) = id_text_between_first_two_blankish_lines(string_from_file)
+        if start_index is None:
+            print("No match found. Invalid file.")
+        elif end_index is not None:
+            print(f"Desired text spans {start_index}–{end_index}:")
+            print_selected_string(string_from_file[start_index: end_index:])
+        else:
+            print(f"Desired text begins at {start_index} and runs till the end of the file:")
+            print_selected_string(string_from_file[start_index::])
+
+    Designed to be tested and demo-ed with the program <run_id_text_between_first_two_blankish_lines.py> and files in:
+        jdr-utilities/tests/run_id_text_between_first_two_blankish_lines
+    in the repository:
+        https://github.com/jimratliff/jdr-python-utilities
+
+    Context (the application for which this was developed): A standards-conformant PGN file has:
+    (a) headers for game 1
+    (b) a blank line
+    (c) movetext for game 1
+    (d) a blank line
+    (e) headers for game 2
+    etc.
+
+    When passed string_from_file, this function determines the start and stop indices such that the slice
+    `string_from_file[start: end::]` contains exactly the desired movetext for game 1 in component (c) above.
+
+    The PGN “output standard” requires that “a single blank line appears after the last of the tag pairs to conclude the
+    tag pair section. This helps simple scanning programs to quickly determine the end of the tag pair section and the
+    beginning of the movetext section.”
+    http://www.saremba.de/chessgml/standards/pgn/pgn-complete.htm#c8.1.1
+
+    This implementation relaxes this standard in two ways:
+    (a) permits a “blank-ish” line (one with non-newline whitespace between two consecutive newline characters) rather
+    than requires a blank line.
+    (b) permits multiple consecutive blank-ish lines even when the standard specifies a single blank line.
+
+    (This is in the spirit of being more permissive on input than output.)
+
+    Although the PGN standard requires that the blank line be strictly empty, this function accepts other whitespace
+    characters on the blank line. (Such a line is “blank-ish.”)
+
+    Note: Because this is implemented with a regex that is greedy, it will gobble up as many consecutive blank-ish lines
+    in component (b) as exist. In other words, even though such a file strictly violates the standard (by having more 
+    that one blank line after the headers), it will not create an error. 
+    """
+
+    # Define regex pattern
+    # Regex pattern matches a string characterized by:
+    # (a) a newline (\n) character followed by
+    # (b) zero or more (“*” quantifier) whitespace (\s) characters, followed by
+    #       Because the “*” isn’t followed immediately by a “?”, this is greedy. Thus it will keep gobbling, even if
+    #       it encounters a newline, as long as there is a later newline still within the matching text (i.e., with 
+    #       nothing but whitespace preceding it). Greediness is relevant here because the outer delimiting characters
+    #       (viz., newline chars) also themselves qualify as whitespace (the filling between the newline bread in this
+    #       sandwich).
+    # (c) a newline character OR end of string ($)
+    #
+    # The prefixing “r” specifies that regex_pattern is a “raw string” and thus backslashes are not seen as 
+    # Python escape characters. See https://docs.python.org/3/howto/regex.html#the-backslash-plague
+
+    regex_pattern = r"\n\s*(\n|$)"
+
+    # I compile the regex because I believe only the compiled regular expression object can be used in the below syntax:
+    #       for match in compiled_regex_pattern.finditer(string_from_file):
+    # Otherwise, I think you'd have to use something (untested) like:
+    #       iterable = re.match(regex_pattern, string_from_file)
+    compiled_regex_pattern = re.compile(regex_pattern)
+
+    # Return an iterator yielding match objects over all non-overlapping matches for the RE pattern in string.
+    # See https://docs.python.org/3/library/re.html#re.finditer
+    # re.finditer(pattern, string, flags=0)
+
+    iterator_of_matches = compiled_regex_pattern.finditer(string_from_file)
+    # First match
+    match = next(iterator_of_matches, None)
+    if match is not None:
+        # The end of the first match is the beginning of the desired text. (Recall that the end_index of the first
+        # match is not actually reached by the first match; thus it is the beginning of the following text.)
+        start_index = match.end()
+        # Second match
+        match = next(iterator_of_matches, None)
+        if match is not None:
+            # The beginning of the second match is one character after the end of the desired movetext, thus the second
+            # index of the slice for the desired text is precisely the beginning of the second match (because the
+            # slice will stop before reaching the second index).
+            end_index = match.start()
+        else:
+            end_index = None
+    else:
+        start_index = None
+        end_index = None
+
+    return (start_index, end_index)
--- src/pgn4people_poc/process_pgn_file.py
+++ src/pgn4people_poc/process_pgn_file.py
@@ -9,6 +9,7 @@ import re
 from . import constants
 from . error_processing import print_fatal_error_exit_without_traceback, pgn_error_fatal_error
 from . jdr_utilities import check_CLI_for_user_file
+from . jdr_utilities import id_text_between_first_two_blankish_lines
 from . strip_balanced_braces import strip_balanced_braces_from_string
 
 def acquire_tokenized_pgnstring():
@@ -16,6 +17,7 @@ def acquire_tokenized_pgnstring():
     Get string of PGN from either (a) file specified by user in command line or (b) a built-in PGN file,
     strips the headers, and tokenizes the remaining movetext.
     """
+
     # Checks command line to see whether user specified her own PGN file to view
     # user_pgn_fileobject will be either (a) a file object or (b) None
     user_pgn_filepath = check_CLI_for_user_file()
@@ -111,42 +113,62 @@ def find_next_blank_line(string, index_to_start):
     index_of_first_newline_char_of_a_pair = string.find("\n\n", index_to_start)
     return index_of_first_newline_char_of_a_pair
 
+
 def strip_headers_from_pgn_file(string_read_from_file, pgn_source):
-    """
-    Takes string read from PGN file and prepares it for tokenizing by:
-        Skips over initial set of headers, followed by a blank line. The next text is the beginning of the relevant
-            movetext.
-        Searches for a second game in the PGN by searching for a subsequent blank line.
-        Returns the text between the beginning of the relevant movetext and any subsequent blank line.
-    pgn_source is passed here solely to allow it to be used in error messages. (Seems less than ideal.)
-    """
+    (start_index, end_index) = id_text_between_first_two_blankish_lines(string_read_from_file)
 
-    # Search for first blank line, which should be the line immediately following the series of headers
-    index_of_first_newline_of_a_consecutive_pair = find_next_blank_line(string_read_from_file, 0)
-
-    if index_of_first_newline_of_a_consecutive_pair == -1:
-        # Two consecutive newline characters not found
-        pgn_file_no_blank_line_after_headers(pgn_source)
-        # pgn_error_fatal_error("No blank line (two consecutive newline characters) found.", pgn_source)
-        # raise ReportError("Error in PGN: No blank line (two consecutive newline characters) found.")
-
-    # Index of first character after the pair of consecutive newline characters is two characters beyond the
-    # occurrence of the first of the pair of newline characters
-    index_of_first_char_after_blank_line = index_of_first_newline_of_a_consecutive_pair + 2
-
-    # Search for a subsequent blank line separating the first game from a second game
-    index_subsequent_blank_line = find_next_blank_line(string_read_from_file, index_of_first_char_after_blank_line)
-
-    # The desired substring is a slice
-    # The end of the slice is either (a) the first newline char of a pair of consecurity newline chars or (b) is -1.
-    # because find_next_blank_line didn't find a next blank line. In that case, the -1 as the end of the slice indicates
-    # the last character of the string.
-    movetext_string = string_read_from_file[index_of_first_char_after_blank_line:index_subsequent_blank_line:]
-
+    if start_index is None:
+        pgn_error_no_blank_line_after_headers(pgn_source)
+    
+    if end_index is None:
+        movetext_string = string_read_from_file[start_index::]
+    else:
+        movetext_string = string_read_from_file[start_index: end_index:]
+    
     # Remove any remaining leading white space
     movetext_string = movetext_string.lstrip()
 
     return movetext_string
+    
+
+
+
+# def strip_headers_from_pgn_file(string_read_from_file, pgn_source):
+#     """
+#     Takes string read from PGN file and prepares it for tokenizing by:
+#         Skips over initial set of headers, followed by a blank line. The next text is the beginning of the relevant
+#             movetext.
+#         Searches for a second game in the PGN by searching for a subsequent blank line.
+#         Returns the text between the beginning of the relevant movetext and any subsequent blank line.
+#     pgn_source is passed here solely to allow it to be used in error messages. (Seems less than ideal.)
+#     """
+
+#     # Search for first blank line, which should be the line immediately following the series of headers
+#     index_of_first_newline_of_a_consecutive_pair = find_next_blank_line(string_read_from_file, 0)
+
+#     if index_of_first_newline_of_a_consecutive_pair == -1:
+#         # Two consecutive newline characters not found
+#         pgn_error_no_blank_line_after_headers(pgn_source)
+#         # pgn_error_fatal_error("No blank line (two consecutive newline characters) found.", pgn_source)
+#         # raise ReportError("Error in PGN: No blank line (two consecutive newline characters) found.")
+
+#     # Index of first character after the pair of consecutive newline characters is two characters beyond the
+#     # occurrence of the first of the pair of newline characters
+#     index_of_first_char_after_blank_line = index_of_first_newline_of_a_consecutive_pair + 2
+
+#     # Search for a subsequent blank line separating the first game from a second game
+#     index_subsequent_blank_line = find_next_blank_line(string_read_from_file, index_of_first_char_after_blank_line)
+
+#     # The desired substring is a slice
+#     # The end of the slice is either (a) the first newline char of a pair of consecurity newline chars or (b) is -1.
+#     # because find_next_blank_line didn't find a next blank line. In that case, the -1 as the end of the slice indicates
+#     # the last character of the string.
+#     movetext_string = string_read_from_file[index_of_first_char_after_blank_line:index_subsequent_blank_line:]
+
+#     # Remove any remaining leading white space
+#     movetext_string = movetext_string.lstrip()
+
+#     return movetext_string
 
 
 def tokenize_pgnstring(pgnstring):
@@ -220,7 +242,7 @@ def pgn_file_not_found_fatal_error(user_pgn_filepath, original_error_message):
     print_fatal_error_exit_without_traceback(error_message)
 
 
-def pgn_file_no_blank_line_after_headers(pgn_source):
+def pgn_error_no_blank_line_after_headers(pgn_source):
     errmsg_list = []
     errmsg_list.append("No blank line found after headers.\n")
     # errmsg_list.append("No blank line (two consecutive newline characters) found after headers.\n")
