ecbd1a59e2f429cdc2638d8753466d0b45a9ee3a
Jim Ratliff <jim@jim-ratliff.name>
2022-06-26 07:10:32 -0700

    Replace sys.exit(main()) with raise SystemExit(main())

--- src/pgn4people_poc/__main__.py
+++ src/pgn4people_poc/__main__.py
@@ -1,14 +1,12 @@
 """
 Entry point for pgn4people_poc package when executed as python -m pgn4people_poc
 See, e.g., https://docs.python.org/3/library/__main__.html
+See also https://www.youtube.com/watch?v=ZbeSPc5wL0g
 """
 
-import sys
 
 from . pgn4people_CLI import main
 
 
 if __name__ == "__main__":
-    sys.exit(main())
-
-
+    raise SystemExit(main())
